4a113373572c8ebd00447fe9555097348cdd8a45
liamsmith3194 <smith.liam1994@gmail.com>
2022-06-16 14:57:14 +0000

    quantity buttons working in product detail template using includes script

--- bag/views.py
+++ bag/views.py
@@ -12,19 +12,19 @@ def add_to_bag(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
-    size = None
-    if 'product_size' in request.POST:
-        size = request.POST['product_size']
+    clothes_size = None
+    if 'clothes_size' in request.POST:
+        clothes_size = request.POST['clothes_size']
     bag = request.session.get('bag', {})
 
-    if size:
+    if clothes_size:
         if item_id in list(bag.keys()):
-            if size in bag[item_id]['items_by_size'].keys():
-                bag[item_id]['items_by_size'][size] += quantity
+            if clothes_size in bag[item_id]['items_by_size'].keys():
+                bag[item_id]['items_by_size'][clothes_size] += quantity
             else:
-                bag[item_id]['items_by_size'][size] = quantity
+                bag[item_id]['items_by_size'][clothes_size] = quantity
         else:
-            bag[item_id] = {'items_by_size': {size: quantity}}
+            bag[item_id] = {'items_by_size': {clothes_size: quantity}}
     else:
         if item_id in list(bag.keys()):
             bag[item_id] += quantity
@@ -33,4 +33,3 @@ def add_to_bag(request, item_id):
 
     request.session['bag'] = bag
     return redirect(redirect_url)
-    
