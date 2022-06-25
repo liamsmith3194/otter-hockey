7d8d543a9653547fbf9c8fb434b1e5fde9e319fd
liamsmith3194 <smith.liam1994@gmail.com>
2022-06-25 14:13:24 +0000

    standard delivery charge instead of percentage

--- otter_hockey/settings.py
+++ otter_hockey/settings.py
@@ -161,7 +161,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Variables
 
 FREE_DELIVERY_THRESHOLD = 100
-STANDARD_DELIVERY_PERCENTAGE = 10
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
