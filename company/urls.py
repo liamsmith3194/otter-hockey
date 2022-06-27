09b8423d35f398e4f3bda6236532fc0198ffcfaa
liamsmith3194 <smith.liam1994@gmail.com>
2022-06-27 20:05:49 +0000

    Privacy policy page set up with views, url and template

--- company/urls.py
+++ company/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('about', views.about, name='about'),
+    path('privacy', views.privacy, name='privacy'),
 ]
--- company/views.py
+++ company/views.py
@@ -2,7 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def about(request):
     """ A view to return the about page"""
-    
+
     return render(request, 'about/about.html')
+
+
+def privacy(request):
+    """ A view to return the privacy policy page"""
+
+    return render(request, 'privacy/privacy.html')
