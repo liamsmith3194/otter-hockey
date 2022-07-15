24296db847d07103d2787434cae94ada8547e262
liamsmith3194 <smith.liam1994@gmail.com>
2022-07-15 17:49:26 +0000

    checkout app created with models for order and line r and line item

--- bag/views.py
+++ bag/views.py
@@ -13,7 +13,7 @@ def view_bag(request):
 
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
-    
+
     product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
@@ -31,14 +31,17 @@ def add_to_bag(request, item_id):
                                  (f'Updated size {clothes_size.upper()} ' f'{product.name} quantity to ' f'{bag[item_id]["items_by_size"][clothes_size]}'))
             else:
                 bag[item_id]['items_by_size'][clothes_size] = quantity
-                messages.success(request, (f'Added size {clothes_size.upper()} ' f'{product.name} to your basket'))
+                messages.success(
+                    request, (f'Added size {clothes_size.upper()} ' f'{product.name} to your basket'))
         else:
             bag[item_id] = {'items_by_size': {clothes_size: quantity}}
-            messages.success(request, (f'Added size {clothes_size.upper()} ' f'{product.name} to your basket'))
+            messages.success(
+                request, (f'Added size {clothes_size.upper()} ' f'{product.name} to your basket'))
     else:
         if item_id in list(bag.keys()):
             bag[item_id] += quantity
-            messages.success(request, (f'Updated {product.name} ' f'quantity to {bag[item_id]}'))
+            messages.success(
+                request, (f'Updated {product.name} ' f'quantity to {bag[item_id]}'))
         else:
             bag[item_id] = quantity
             messages.success(request, f'Added {product.name} to your basket')
@@ -70,7 +73,7 @@ def add_to_bag(request, item_id):
 
 def adjust_bag(request, item_id):
     """Adjust the quantity of the specified product to the specified amount"""
-    
+
     product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     clothes_size = None
@@ -81,16 +84,19 @@ def adjust_bag(request, item_id):
     if clothes_size:
         if quantity > 0:
             bag[item_id]['items_by_size'][clothes_size] = quantity
-            messages.success(request, f'Updated size {clothes_size.upper()} {product.name} quantity to {bag[item_id]["items_by_size"][clothes_size]}')
+            messages.success(
+                request, f'Updated size {clothes_size.upper()} {product.name} quantity to {bag[item_id]["items_by_size"][clothes_size]}')
         else:
             del bag[item_id]['items_by_size'][clothes_size]
             if not bag[item_id]['items_by_size']:
                 bag.pop(item_id)
-            messages.success(request, f'Removed size {clothes_size.upper()} {product.name} from your bag')
+            messages.success(
+                request, f'Removed size {clothes_size.upper()} {product.name} from your bag')
     else:
         if quantity > 0:
             bag[item_id] = quantity
-            messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')
+            messages.success(
+                request, f'Updated {product.name} quantity to {bag[item_id]}')
         else:
             bag.pop(item_id)
             messages.success(request, f'Removed {product.name} from your bag')
@@ -113,7 +119,8 @@ def remove_from_bag(request, item_id):
             del bag[item_id]['items_by_size'][clothes_size]
             if not bag[item_id]['items_by_size']:
                 bag.pop(item_id)
-            messages.success(request, f'Removed size {clothes_size.upper()} {product.name} from your bag')
+            messages.success(
+                request, f'Removed size {clothes_size.upper()} {product.name} from your bag')
         else:
             bag.pop(item_id)
             messages.success(request, f'Removed {product.name} from your bag')
@@ -124,4 +131,3 @@ def remove_from_bag(request, item_id):
     except Exception as e:
         messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
-    
--- otter_hockey/settings.py
+++ otter_hockey/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'company',
     'products',
     'bag',
+    'checkout',
     'profiles',
     
     'crispy_forms',
