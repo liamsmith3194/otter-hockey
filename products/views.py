16b3dfc47d466cd38b86f7c4f98805aa7460f0bb
liamsmith3194 <smith.liam1994@gmail.com>
2022-07-30 12:01:04 +0000

    set up email configuration

--- products/views.py
+++ products/views.py
@@ -45,7 +45,7 @@ def all_products(request):
     current_sorting = f'{sort}_{direction}'
 
     context = {
-        'category': products[0].category,
+        # 'category': category,
         'products': products,
         'search_term': query,
         'current_categories': categories,
